50b982c7bf641a543a3de38662cab7522fe8aaa5
Connor Conway <connor.conway@cloudhouse.com>
2020-12-01 13:38:09 +0000

    Adds wordwrap implementation

---
+++ WordWrap/WordWrap.Test/WrapperTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace WordWrap.Test
+{
+    [TestFixture]
+    public class WrapperTest
+    {
+        [Test]
+        public void Wrap()
+        {
+            Assert.Throws<ArgumentException>(() => Wrapper.Wrap("something here", 0), "Must split on a valid column number");
+
+            Assert.AreEqual("", Wrapper.Wrap("", 1));
+
+            Assert.AreEqual("one", Wrapper.Wrap("one", 4));
+            Assert.AreEqual("one", Wrapper.Wrap("one", 3));
+            Assert.AreEqual($"on{Environment.NewLine}e{Environment.NewLine}", Wrapper.Wrap("one", 2));
+            Assert.AreEqual($"o{Environment.NewLine}n{Environment.NewLine}e{Environment.NewLine}", Wrapper.Wrap("one", 1));
+
+            Assert.AreEqual($"one{Environment.NewLine}two{Environment.NewLine}", Wrapper.Wrap("one two", 4));
+            Assert.AreEqual($"one{Environment.NewLine}two{Environment.NewLine}", Wrapper.Wrap("one two", 3));
+        }
+    }
+}
---
+++ WordWrap/WordWrap/Wrapper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace WordWrap
+{
+    public class Wrapper
+    {
+        public static string Wrap(string words, int columnToSplit)
+        {
+            if (columnToSplit == 0)
+                throw new ArgumentException("Must split on a valid column number");
+
+            if (words.Length > columnToSplit)
+            {
+                var k = 0;
+                var output = words
+                    .ToLookup(c => Math.Floor((decimal) (k++ / columnToSplit)))
+                    .Select(e => new string(e.ToArray()));
+
+                var stringbuilder = new StringBuilder();
+                foreach (var part in output)
+                {
+                    if (part.EndsWith(" "))
+                    {
+                        var updated = part.Replace(" ", Environment.NewLine);
+                        stringbuilder.Append(updated);
+                    }
+                    else
+                    {
+                        stringbuilder.Append(part + Environment.NewLine);
+                    }
+                }
+
+                return stringbuilder.ToString();
+
+            }
+            return words;
+        }
+    }
+}
